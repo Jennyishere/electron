5fed9149b354164213abc5a05e96804e653df69c
Jennyishere <853297695@qq.com>
2020-08-14 13:46:59 +0800

    更新

--- main.js
+++ main.js
@@ -1,66 +1,68 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow,Notification,ipcMain} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Notification, ipcMain } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-      // preload: path.join(__dirname, 'preload.js')
-      webPreferences:{
-        nodeIntegration:true
-      }
-    
-  })
+    // preload: path.join(__dirname, 'preload.js')
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  handleIPC()
+  mainWindow.webContents.openDevTools();
+  handleIPC();
 }
-function handleIPC(){
-  ipcMain.handle('work-notification',async()=>{
-    let res=await new Promise((resolve, reject) => {
-      let notification= new Notification({
-        title:'任务结束',
-        body:'是否开始休息',
-        actions:[{text:'开始休息',type:'button'}], // macOS -
-        closeButtonText:'继续工作'  // macOS -
-      })
-      notification.show()
-      notification.on('action', () => {
-        resolve('rest')
-    })
-    notification.on('close', () => {
-      console.log('close');
-        resolve('work')
-    })
-    })
-    return res
-  })
+function handleIPC() {
+  ipcMain.handle("work-notification", async (event, ...args) => {
+    console.log(args);
+    let res = await new Promise((resolve, reject) => {
+      let notification = new Notification({
+        title: args[0] == "work" ? "工作结束" : "休息结束",
+        body: args[0] == "work" ? "开始休息" : "开始工作",
+        // actions: [{ text: "开始休息", type: "button" }], // macOS -
+        // closeButtonText: "继续工作", // macOS -
+      });
+      notification.show(); //弹出通知
+      resolve(args[0]);
+      //  Mac系统是有两个按钮
+      // notification.on("action", () => {
+      //   resolve("rest");
+      // });0
+      // notification.on("close", () => {
+      //   console.log("close");
+      //   resolve("work");
+      // });
+    });
+    return res;
+  });
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
+  createWindow();
+
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,35 +1,76 @@
-const Timer = require('timer.js')
-const {ipcRenderer} =require('electron')  //引入 它提供了有限的方法，你可以从渲染进程向主进程发送同步或异步消息. 也可以收到主进程的相应.
-function startWork(){
-    let workTime = new Timer({
-        ontick: (ms) => {
-           
-            updateTime(ms)
-        },
-        onend: () => {
-            notification()
-        }
-    })
-    workTime.start(2)
-}
-function updateTime(ms){
-    let timerCntainer=document.getElementById('container')
-    let ss = (ms / 1000).toFixed(0) % 60
-    let mm = ((ms / 1000).toFixed(0) / 60).toFixed(0)
-    timerCntainer.innerText = `${mm.toString().padStart(2, 0)}: ${ss.toString().padStart(2, 0)}`
+const Timer = require("timer.js");
+const { ipcRenderer } = require("electron"); //引入 它提供了有限的方法，你可以从渲染进程向主进程发送同步或异步消息. 也可以收到主进程的相应.
+const ProgressBar = require("progressbar.js/dist/progressbar.js");
+// let progressBar = new ProgressBar.Circle("#timer-container", {
+//   strokeWidth: 2,
+//   color: "#F44336",
+//   trailColor: "#eee",
+//   trailWidth: 1,
+//   svgStyle: null,
+// });
+function startWork(time, key) {
+  let workTime = new Timer({
+    //   倒计时开始
 
+    ontick: (ms) => {
+      updateTime(ms);
+
+      //   if (key == "work") {
+      //     switchButton.innerText = "正在工作";
+      //   } else {
+      //     switchButton.innerText = "正在休息";
+      //   }
+    },
+    onend: () => {
+      // 倒计时结束发送通知
+      console.log("key", key);
+      if (key == "work") {
+        switchButton.innerText = "开始休息";
+      } else {
+        switchButton.innerText = "开始工作";
+      }
+      notification(key);
+    },
+  });
+  workTime.start(time); //调用方法
 }
-async function notification(){
-    // 异步请求/响应样式的IPC添加了ipcRenderer.invoke() 
-    console.log(ipcRenderer);
-    let res= await ipcRenderer.invoke('work-notification')
-    console.log('res',res);
-  if(res==='rest'){
-setTimeout(() => {
-    alert('休息')
-}, 5000);
-  }else if(res==='work'){
-      startWork()
+let switchButton = document.getElementById("switch-button");
+function updateTime(ms) {
+  let timerCntainer = document.getElementById("container");
+  let timerContainer = document.getElementById("timer-container");
+
+  let ss = (ms / 1000).toFixed(0) % 60;
+  let mm = ((ms / 1000).toFixed(0) / 60).toFixed(0);
+  timerCntainer.innerText = `${mm.toString().padStart(2, 0)}: ${ss.toString().padStart(2, 0)}`;
+}
+async function notification(key) {
+  // 异步请求/响应样式的IPC添加了ipcRenderer.invoke()
+
+  let res = await ipcRenderer.invoke("work-notification", key); //向主进程发送事件
+  console.log("res", res);
+  if (res === "rest") {
+    // alert("休息");
+    // if (window.confirm("确认休息吗?")) {
+    //   let timerCntainer = document.getElementById("container");
+    //   timerCntainer.innerText = "正在休息....";
+    //   setTimeout(() => {
+    //     startWork();
+    //   }, 5000);
+    // } else {
+    //   startWork();
+    // }
+    // startWork(3, "rest");
+  } else if (res === "work") {
+    // startWork(5, "work");
   }
 }
-startWork()
+switchButton.onclick = function () {
+  if (this.innerText === "开始工作") {
+    startWork(5, "work");
+  } else if (this.innerText === "开始休息") {
+    startWork(3, "rest");
+  } else {
+    workTimer.stop();
+  }
+};
+// startWork();
